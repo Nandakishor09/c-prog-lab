919d48fbc473b5794871a6b2003bd0b69b2e6a57
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-09-06 21:06:49 +0530

    Update ex-3-1.c

--- ex-3-1.c
+++ ex-3-1.c
@@ -9,10 +9,11 @@ int main(){
     if ( num == 0)
         printf("Entered number is 0");
     else if ( num % 2 == 0)
-        printf("Enter number is a even number");
+        printf("Entered number is a even number");
     else
-        printf("Enter number is a odd number");
+        printf("Entered number is a odd number");
 
     return 0;
 }
+
 
