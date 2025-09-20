782f96c6f7f7ef487ac14c65a9d122e1b2d9110b
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-09-20 12:32:26 +0530

    Add files via upload

---
+++ ex-4-4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    int i,j;
+    int matrix[100][100];
+    int row,col,zeros;
+    int even = 0, odd = 0;
+
+    printf("Enter no of rows: ");
+    scanf("%d", &row);
+    printf("Enter no of cols: ");
+    scanf("%d", &col);
+
+    for( i = 0; i < row; i++){
+        for ( j = 0; j < col; j++){
+            printf("Enter a number: ");
+            scanf("%d", &matrix[i][j]);
+            if( matrix[i][j] == 0){
+                zeros++;
+            }
+            else if ( matrix[i][j] % 2 == 0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+    }
+
+    for( i = 0; i < row; i++){
+        for ( j = 0; j < col; j++){
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("No of even numbers in the array: %d\n", even);
+    printf("No of odd numbers in the array: %d\n", odd);
+    printf("No of even zeros in the array: %d\n", zeros);
+
+}
+
